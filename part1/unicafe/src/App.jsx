53144b9149fd4c844a0c0525c3ecf9bea7a36691
Yunpeng Dong <yunpengdong@192.168.0.104>
2024-02-15 16:17:59 +0800

    Complete unicafe exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Button handles the functionality of each feedback submission button.
+const Button = (props) => <button onClick={props.onSmash}>{props.text}</button>
 
-function App() {
-  const [count, setCount] = useState(0)
+// StatisticLine for displaying a single statistic, e.g. the average score.
+const StatisticLine = (props) => <tr><td>{props.text}</td><td>{props.value}</td></tr>
 
+const Statistics = (props) => {
+  let sumOfFeedback = props.good + props.neutral + props.bad
+  
+  const calAverage = () => (props.good - props.bad) / sumOfFeedback
+  const calPositiveRate = () => ((props.good / sumOfFeedback) * 100) + " %"
+  
+  if (sumOfFeedback === 0) {
+    return(
+      <>
+      <p>No feedback given</p>
+      </>
+    )
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+  <>
+  <table>
+    <tbody>
+    <StatisticLine text="good" value ={props.good} />
+    <StatisticLine text="neutral" value ={props.neutral} />
+    <StatisticLine text="bad" value ={props.bad} />
+    <StatisticLine text="average" value ={calAverage()} />
+    <StatisticLine text="positive" value ={calPositiveRate()} />
+    </tbody>
+  </table>
+  </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <p>
+        <Button text="good" onSmash={()=>setGood(good+1)}/>
+        <Button text="neutral" onSmash={()=>setNeutral(neutral+1)}/>
+        <Button text="bad" onSmash={()=>setBad(bad+1)}/>
       </p>
-    </>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
   )
 }
 
